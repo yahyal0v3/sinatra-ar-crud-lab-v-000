ef3032d011302652ce44eac702030297d74a355b
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-15 21:25:39 +0000

    automatically backed up by learn

--- db/migrate/001_create_posts.rb
+++ db/migrate/001_create_posts.rb
@@ -1,8 +1,8 @@
-class Create_Posts < ActiveRecord::Migration[4.2]
+class CreatePosts < ActiveRecord::Migration[4.2]
   def change
     create_table :posts do |t|
       t.string :name
       t.string :content
     end
   end
-end 
+end
