9bef3af6ca593bce6d80bbdad2049829fd43a008
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-15 21:25:57 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "content"
+  end
 
 end
