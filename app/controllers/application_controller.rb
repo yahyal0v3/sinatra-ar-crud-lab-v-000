abac2739f150cf4c9570cb4058f7611b6a150a5d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-16 04:05:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,9 +43,8 @@ class ApplicationController < Sinatra::Base
   end
 
    delete '/posts/:id/delete' do
-     post = Post.find(params[:id])
-     @name = post.name
-     post.delete
+     @post = Post.find(params[:id])
+     @post.delete
      erb :delete
    end
 end
